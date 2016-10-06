dd4e7e0109460b5763a6c4b0e1e63ab665dc19fd
xinnix <xinnix2000@gmail.com>
2016-10-06 21:27:33 +0800

    add signup page

--- src/config/express.js
+++ src/config/express.js
@@ -1,25 +1,28 @@
 const wechatRoute = require('../routes/wechat.server.routes');
 const adminRoute = require('../routes/admin.server.routes');
-const frontendRoute = require('../routes/frontend.server.routes');
+// const frontendRoute = require('../routes/frontend.server.routes');
 const express = require('express');
 const bodyParser = require('body-parser');
+const swig = require('swig');
 
 
 module.exports = () => {
   const app = express();
+  app.engine('html', swig.renderFile);
+  app.set('view engine', 'html');
   app.set('views', './src/views');
   app.use(bodyParser.urlencoded({
     extended: true,
   }));
   app.use(bodyParser.json());
   app.use(express.static('public'));
-  app.all('*',function (req, res, next) {
+  app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-    if (req.method == 'OPTIONS') {
-      res.send(200); /让options请求快速返回/
+    if (req.method === 'OPTIONS') {
+      res.send(200);
     }
     else {
       next();
@@ -27,7 +30,7 @@ module.exports = () => {
   });
 
   app.use('/wechat', wechatRoute);
-  app.use('/admin',  adminRoute);
+  app.use('/admin', adminRoute);
   // app.use('/frontend', frontendRoute);
 
   return app;
--- src/controllers/admin.server.controllers.js
+++ src/controllers/admin.server.controllers.js
@@ -1,29 +1,67 @@
-const getErrorMessage = require('./core/errors.server.controllers').getErrorMessage;
+// const getErrorMessage = require('./core/errors.server.controllers').getErrorMessage;
 
 
-const path = require('path');
+// const path = require('path');
 const webapi = require('../config/wechatAPI').webapi;
 const request = require('superagent');
 const config = require('../config/config');
 
-const fs = require('fs');
+// const fs = require('fs');
 const co = require('co');
-const userHelper = require('../helper/userHelper.server');
+// const userHelper = require('../helper/userHelper.server');
 
-function test(req,res){
+function test(req, res) {
   res.send('ok');
 }
 
-function login(req, res) {
-  co(function *(){
-    try {
-      const username = req.body.username;
-      const password = req.body.password;
-      const token = yield authBaseServer(username,password);
-      res.send(token.body);
-    } catch (err) {
-      console.log(err);
-    }
+// function login(req, res) {
+//   co(function *(){
+//     try {
+//       const username = req.body.username;
+//       const password = req.body.password;
+//       const token = yield authBaseServer(username,password);
+//       res.send(token.body);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
+// }
+function getUserOpenIdByWeb(code) {
+  return new Promise((resolve, reject) => {
+    webapi.getAccessToken(code, (err, result) => {
+      if (err)reject(err);
+      resolve(result.data.openid);
+      // var accessToken = result.data.access_token;
+      // var openid = result.data.openid;
+    });
+  });
+}
+// 当接收到用户发来的消息时，判断数据库中是否有用户信息，若无，则根据openid从微信服务器获取
+function getUserInfoByOpenId(openid) {
+  return new Promise((resolve, reject) => {
+    webapi.getUser(openid, (err, result) => {
+      if (err)reject(err);
+      resolve(result);
+    });
+  });
+}
+
+function authBaseServer(user) {
+  return new Promise((resolve, reject) => {
+    request
+    .post(config.baseServer.baseRoute + config.baseServer.authRoute)
+    .send({
+      providerData: user,
+      client_id: 'kf-app',
+      client_secret: 'prometheus',
+      grant_type: 'password',
+    })
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((err) => {
+      reject(err);
+    });
   });
 }
 
@@ -33,49 +71,10 @@ function wechat(req, res) {
       const openid = yield getUserOpenIdByWeb(req.query.code);
       const user = yield getUserInfoByOpenId(openid);
       const result = yield authBaseServer(user);
-      res.send(result.body);
+      res.render('./signup', { baseinfo: result.body, user });
     } catch (err) {
       console.log(err);
     }
-  });
-}
-function getUserOpenIdByWeb(code) {
-  return new Promise((resolve, reject) => {
-    webapi.getAccessToken(code, (err, result) => {
-      if (err)reject(err);
-      resolve(result.data.openid);
-      // var accessToken = result.data.access_token;
-      // var openid = result.data.openid;
-    });
-
-  });
-}
-// 当接收到用户发来的消息时，判断数据库中是否有用户信息，若无，则根据openid从微信服务器获取
-function getUserInfoByOpenId(openid) {
-  return new Promise((resolve, reject) => {
-     webapi.getUser(openid, function (err, result) {
-       if(err)reject(err);
-       resolve(result);
-    });
-  });
-}
-
-function authBaseServer(user) {
-  return new Promise((resolve, reject) => {
-    request
-    .post(config.baseServer.baseRoute+config.baseServer.authRoute)
-    .send({
-        providerData: user,
-        client_id: "kf-app",
-        client_secret: "prometheus",
-        grant_type: 'password',
-    })
-    .then((res) => {
-      resolve(res);
-    })
-    .catch((err) => {
-      reject(err);
-    });
   });
 }
 // function materialList(req, res) {
@@ -163,11 +162,9 @@ function authBaseServer(user) {
 //     }
 //   });
 // }
-
-
-
 module.exports = {
-  login,
+  // login,
   wechat,
   test,
+  signup,
 };

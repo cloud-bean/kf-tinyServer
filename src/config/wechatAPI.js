0c305d3814dbb2494b6a06425e52b899d677178a
xinnix <xinnix2000@gmail.com>
2017-02-17 16:03:20 +0800

    resolve

--- src/config/wechatAPI.js
+++ src/config/wechatAPI.js
@@ -18,20 +18,20 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const webapi = new OAuth(appid, appsecret);
-// const api = new WechatAPI(appid, appsecret);
-const api = new WechatAPI(appid, appsecret, function (callback) {
-  // 传入一个获取全局token的方法
-  console.log('read!!');
-  fs.readFile('access_token.txt', 'utf8', function (err, txt) {
-    if (err) {return callback(err);}
-    callback(null, JSON.parse(txt));
-  });
-}, function (token, callback) {
-  // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-  // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-  console.log('write!!');
-  fs.writeFile('access_token.txt', JSON.stringify(token), callback);
-});
+const api = new WechatAPI(appid, appsecret);
+// const api = new WechatAPI(appid, appsecret, function (callback) {
+//   // 传入一个获取全局token的方法
+//   console.log('read!!');
+//   fs.readFile('access_token.txt', 'utf8', function (err, txt) {
+//     if (err) {return callback(err);}
+//     callback(null, JSON.parse(txt));
+//   });
+// }, function (token, callback) {
+//   // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+//   // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+//   console.log('write!!');
+//   fs.writeFile('access_token.txt', JSON.stringify(token), callback);
+// });
 
 module.exports = {
   api,

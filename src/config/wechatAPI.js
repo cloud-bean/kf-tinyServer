e157e1ce3ef649004581a94100a79f16803c9baa
xinnix <xinnix2000@gmail.com>
2017-02-17 11:13:14 +0800

    try local store the access token

--- src/config/wechatAPI.js
+++ src/config/wechatAPI.js
@@ -17,20 +17,18 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const webapi = new OAuth(appid, appsecret);
-const api = new WechatAPI(appid, appsecret);
-// const api = new WechatAPI(config.wechatConfig.appid_test, config.wechatConfig.appsecret_test, callback => {
-//   // 传入一个获取全局token的方法
-//   fs.readFile('access_token.txt', 'utf8', (err, txt) => {
-//     if (err) {return callback(err);}
-//     callback(null, JSON.parse(txt));
-//   },
-//   (token, callback1) => {
-//   // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-//   // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-//     fs.writeFile('access_token.txt', JSON.stringify(token), callback1);
-//   }
-// );
-// });
+// const api = new WechatAPI(appid, appsecret);
+const api = new WechatAPI(appid, appsecret, function (callback) {
+  // 传入一个获取全局token的方法
+  fs.readFile('access_token.txt', 'utf8', function (err, txt) {
+    if (err) {return callback(err);}
+    callback(null, JSON.parse(txt));
+  });
+}, function (token, callback) {
+  // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+  // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+  fs.writeFile('access_token.txt', JSON.stringify(token), callback);
+});
 
 module.exports = {
   api,

ef051c95eb226cbaa70421a2ebb7a3bf75b24766
xinnix <xinnix2000@gmail.com>
2017-08-03 16:20:59 +0800

    openid support

--- src/controllers/weapp.server.controllers.js
+++ src/controllers/weapp.server.controllers.js
@@ -4,16 +4,16 @@ const config = require('../config/config');
 function getUserInfo(req, res) {
   const data = req.body;
   const code = data.code;
-  let unionid = '';
   request
   .get('https://api.weixin.qq.com/sns/jscode2session')
   .query({ appid: config.weappConfig.appid, secret: config.weappConfig.appsecret, js_code: code, grant_type: 'authorization_code' }) // query string
   .end(function(err, result){
-    unionid = JSON.parse(result.text).unionid;
+    const unionid = JSON.parse(result.text).unionid;
+    const openid = JSON.parse(result.text).openid;
+
     authBaseServerByUnionId(unionid)
     .then((loginData)=>{
-      const data = Object.assign({},loginData.body,{unionid});
-      console.log('add unionid', data);
+      const data = Object.assign({},loginData.body,{unionid},{openid});
       res.json(data);
     })
     .catch(e => {

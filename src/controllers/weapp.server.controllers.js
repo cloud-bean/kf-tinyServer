559ada089e767aebe2b389a51b74e9c6367feed4
hygkui <hygkui@gmail.com>
2018-12-03 20:21:38 +0800

    keep same name

--- src/controllers/weapp.server.controllers.js
+++ src/controllers/weapp.server.controllers.js
@@ -6,11 +6,11 @@ function getUserInfo(req, res) {
   const data = req.body;
   const code = data.code;
   const appName = data.appName;
-  const { appid, secret } = config.weappConfig[appName];
-  console.log('appName, appid, secret', appName, appid, secret)
+  const { appid, appsecret } = config.weappConfig[appName];
+  console.log('appName, appid, secret', appName, appid, appsecret)
   request
   .get('https://api.weixin.qq.com/sns/jscode2session')
-  .query({ appid, secret, js_code: code, grant_type: 'authorization_code' }) // query string
+  .query({ appid, appsecret, js_code: code, grant_type: 'authorization_code' }) // query string
   .end(function(err, result){
     const unionid = JSON.parse(result.text).unionid;
     const openid = JSON.parse(result.text).openid;

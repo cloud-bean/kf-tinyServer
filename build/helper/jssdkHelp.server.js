d53832a572119e54f8dc5aae5bff8bb63012b96c
hygkui <hygkui@gmail.com>
2019-10-21 17:09:33 +0800

    deploy to new server bridgeWater

--- build/helper/jssdkHelp.server.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=jssdkHelp.server.js.map
--- src/config/config.js
+++ src/config/config.js
@@ -1,36 +1,36 @@
 module.exports = {
   port: process.env.KEEP_PORT || 5555,
   // db: process.env.KEEP_DB_URI || 'mongodb://localhost/keepreading',
-  baseServer:{
-    baseRoute:'http://120.25.227.156:7000/',
-    authRoute:"oauth/wechat/token",
-    weappAuthRoute:"oauth/wepy/token",
-    userRoute:"api/base/users/",
+  baseServer: {
+    baseRoute: 'http://127.0.0.1:7000/',
+    authRoute: 'oauth/wechat/token',
+    weappAuthRoute: 'oauth/wepy/token',
+    userRoute: 'api/base/users/'
   },
-  oauth:{
-    client_id: "kf-app",
-    client_secret: "prometheus",
-    grant_type: 'password',
+  oauth: {
+    client_id: 'kf-app',
+    client_secret: 'prometheus',
+    grant_type: 'password'
   },
   wechatConfig: {
     token: 'helloworld',
-    appid: 'wxb3762a929cf9e070',
-    appsecret: '0a607b7dc2ff4fa027b8594a99f9e086',
-    appid_test: 'wxf8e294c011cd47e1',
-    appsecret_test: '729b99cfef8f39ec492f4fbaf3576b72',
-    encodingAESKey: 'e4miuSxQNBIAFYuyi5ky0m1R79oV950ijnjKOtPSXYG',
+    appid: 'wx9d399e46dcb4c0f9',
+    appsecret: '5c4c6fcc78e3cd77d3260340cd9b88a4',
+    // appid_test: 'wxf8e294c011cd47e1',
+    // appsecret_test: '729b99cfef8f39ec492f4fbaf3576b72',
+    encodingAESKey: 'unEIGGPCrjm6kTWRUwn00t8dzcKTttTF3qCPAq6VyTd'
   },
   weappConfig: {
     joyBox: {
       appid: 'wx9de05118307b2e44',
-      secret: '034d0167fc7af96e06c76cc54aeeba62',
+      secret: '034d0167fc7af96e06c76cc54aeeba62'
     },
     joyTeacher: {
       appid: 'wx6580d634dd0be91f',
-      secret: 'e534017964bbf60f4bf4975bc0589038',
+      secret: 'e534017964bbf60f4bf4975bc0589038'
     }
   },
   logic: {
-    delayTime: 10000,
-  },
-};
+    delayTime: 10000
+  }
+}
